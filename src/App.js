73f457ac80481bbb0926001a1a859d694d624eac
Lei Hsu <ge159951@gmail.com>
2021-01-27 14:51:48 +0800

    完成靜態UI畫面

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { ReactComponent as DayCloudyIcon } from "./images/day-cloudy.svg";
+import { ReactComponent as RainIcon } from "./images/rain.svg";
+import { ReactComponent as AirFlowIcon } from "./images/airFlow.svg";
+import { ReactComponent as RefreshIcon } from "./images/refresh.svg";
 
+function App() {
+  return (
+    <Container>
+      <WeatherCard>
+        <Location>台北市</Location>
+        <Description>多雲時晴</Description>
+        <CurrentWeather>
+          <Tempreature>
+            23<Celsius>°C</Celsius>
+          </Tempreature>
+          <DayCloudy />
+        </CurrentWeather>
+        <AirFlow>
+          <AirFlowIcon />
+          23 m/h
+        </AirFlow>
+        <Rain>
+          <RainIcon />
+          48%
+        </Rain>
+        <Refresh>
+          最後觀測時間:上午12:03
+          <RefreshIcon />
+        </Refresh>
+      </WeatherCard>
+    </Container>
+  );
+}
+
+// CSS in Js
 const Container = styled.div`
   background-color: #ededed;
   height: 100%;
@@ -8,7 +42,6 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
 `;
-
 const WeatherCard = styled.div`
   position: relative;
   min-width: 360px;
@@ -17,15 +50,80 @@ const WeatherCard = styled.div`
   box-sizing: border-box;
   padding: 30px 15px;
 `;
+//location
+const Location = styled.div`
+  font-size: 28px;
+  color: #212121;
+  margin-bottom: 20px;
+`;
+//description
+const Description = styled.div`
+  font-size: 16px;
+  color: #828282;
+  margin-bottom: 30px;
+`;
+//currentWeather
+const CurrentWeather = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+//temperature
+const Tempreature = styled.div`
+  color: #757575;
+  font-size: 96px;
+  font-weight: 300;
+  display: flex;
+`;
+const Celsius = styled.div`
+  font-weight: normal;
+  font-size: 42px;
+`;
+const AirFlow = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+  margin-bottom: 20px;
+  svg {
+    width: 25px;
+    height: auto;
+    margin-right: 30px;
+  }
+`;
 
-function App() {
-  return (
-    <Container>
-      <WeatherCard>
-        <h1>Weather</h1>
-      </WeatherCard>
-    </Container>
-  );
-}
+const Rain = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+  svg {
+    width: 25px;
+    height: auto;
+    margin-right: 30px;
+  }
+`;
 
+const Refresh = styled.div`
+  position: absolute;
+  right: 15px;
+  bottom: 15px;
+  font-size: 12px;
+  display: inline-flex;
+  align-items: flex-end;
+  color: #828282;
+  svg {
+    width: 15px;
+    height: 15px;
+    margin-left: 30px;
+    cursor: pointer;
+  }
+`;
+//修改conponent的css
+const DayCloudy = styled(DayCloudyIcon)`
+  flex-basis: 30%;
+`;
 export default App;
